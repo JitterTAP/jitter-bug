76ba39065c33575e3a76c3de0ce0941dc323f205
jerluc <jeremyalucas@gmail.com>
2016-02-14 13:02:49 -0800

    Murika

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+var jitterbug = new WebSocket('ws://ec2-52-10-33-198.us-west-2.compute.amazonaws.com:15000/jitterdog', []);
+
+jitterbug.onopen = function() {
+  jitterbug.send('HAI');
+};
+
+jitterbug.onmessage = function(event) {
+  console.log('File changed, upgrading');
+  upgradeCSS();
+};
+
+function loadingAnimation(andThen) {
+  var line = new ProgressBar.Line('#progress', {
+      color: '#FCB03C'
+  });
+
+  line.animate(1.0, function() {
+    line.destroy();
+    if (andThen) andThen();
+  });
+}
+
+function upgradeCSS() {
+  loadingAnimation(function() {
+    document.getElementById('mainstyles').href = 'assets/css/main.css';
+  });
+}
